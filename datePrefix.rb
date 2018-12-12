6bd06fd04507f6051a1b8f6f8e28efcb35b1536f
Uriel Perez <45811683+urieldjperez@users.noreply.github.com>
2018-12-12 10:34:34 -0800

    Inlined the Hash map
    Inlined the Hash map using it's implicit form for readability.

--- datePrefix.rb
+++ datePrefix.rb
@@ -101,19 +101,20 @@ end
 #####################################################################
 
 # lookup table monthName => monthNum
-monthLookup = Hash.new
-monthLookup['jan'] =  1
-monthLookup['feb'] =  2
-monthLookup['mar'] =  3
-monthLookup['apr'] =  4
-monthLookup['may'] =  5
-monthLookup['jun'] =  6
-monthLookup['jul'] =  7
-monthLookup['aug'] =  8
-monthLookup['sep'] =  9
-monthLookup['oct'] = 10
-monthLookup['nov'] = 11
-monthLookup['dec'] = 12
+monthLookup =  {
+  'jan' => 1,
+  'feb' => 2,
+  'mar' => 3,
+  'apr' => 4,
+  'may' => 5,
+  'jun' => 6,
+  'jul' => 7,
+  'aug' => 8,
+  'sep' => 9,
+  'oct' => 10,
+  'nov' => 11,
+  'dec' => 12,
+}
 
 cmdStdOut = ` ls -lT `
 splitTokenStr = "\n"
